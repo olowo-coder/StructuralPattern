98c19ef9d63318ea5a04f79666cb2e27679b7bf3
olowo-coder <lanre75olowo@gmail.com>
2022-06-26 20:14:45 +0100

    decorator pattern

---
+++ src/main/java/decorator/DecoratorDemo.java
@@ -0,0 +1,10 @@
+package decorator;
+
+public class DecoratorDemo {
+    public static void main(String[] args) {
+
+        Sandwich sandwich = new DressingDecorator(new MeatDecorator(new SimpleSandwich()));
+
+        System.out.println(sandwich.make() );
+    }
+}
